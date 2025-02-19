d3dcc00e6e70f6a71c7b2eedaa181ca18f38ff9f
jafarkhan577 <152817161+jafarkhan577@users.noreply.github.com>
2025-02-19 17:55:13 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".buy-now").addEventListener("click", function() {
+    alert("Redirecting to Payment Gateway...");
+    window.location.href = "#"; // Replace with actual payment link
+});
